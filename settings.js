b1f190e73b9c21fe7be59c57287f25cc7d4f9ec4
KyzmkyzTempest <121739095+Zet65@users.noreply.github.com>
2024-10-30 17:06:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Kayla Host'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://db-nailaa:25ro4v19qXrzmua3@cluster0.9eta8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "ICHIKA-ACTIVATOR" //isi apa aja bebas
 global.your_email = "ayaraprojectsgen2@gmail.com" //email
 global.email_password = "@AyaraProject12345" //application password email
